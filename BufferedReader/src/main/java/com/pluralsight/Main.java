302cdaf166bab88bce8c0cf403b4197ca2b5ebf8
agabin <agabin@appdev.yearup.org>
2024-10-09 15:31:27 -0400

    Completed Exercise 3 - BufferedReader

--- BufferedReader/src/main/java/com/pluralsight/Main.java
+++ BufferedReader/src/main/java/com/pluralsight/Main.java
@@ -1,8 +1,38 @@
 package com.pluralsight;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Pattern;
 
-        System.out.println("Hello world!");
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+
+        // create a FileReader object connected to the File
+        FileReader fileReader = new FileReader("employees.csv");
+        // create a BufferedReader to manage input stream
+        BufferedReader bufReader = new BufferedReader(fileReader);
+        bufReader.readLine();
+        String input;
+        // read until there is no more data
+        while((input = bufReader.readLine()) != null) {
+            String[] tokens = input.split(Pattern.quote("|"));
+
+            int employeeId = Integer.parseInt(tokens[0]);
+            String name = tokens[1];
+            float hoursWorked = Float.parseFloat(tokens[2]);
+            double payRate = Double.parseDouble(tokens[3]);
+
+            Employee theEmp = new Employee(employeeId, name, hoursWorked, payRate);
+            displayEmployee(theEmp);
+        }
+        // close the stream and release the resources
+        bufReader.close();
+    }
+
+    private static void displayEmployee(Employee e){
+        System.out.printf("%d:%s Pay this period: %.2f\n", e.getEmployeeId(), e.getName(), e.getGrossPay());
     }
 }
